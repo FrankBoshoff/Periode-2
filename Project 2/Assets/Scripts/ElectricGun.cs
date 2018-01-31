a13862f98ae1f9ce64a8bc2663a301d22980d68f
Frank <Frank_Boshoff@live.nl>
2018-01-31 17:02:00 +0100

    Gunz en slechteriken
    these ricks are bad

--- Project 2/Assets/Scripts/ElectricGun.cs
+++ Project 2/Assets/Scripts/ElectricGun.cs
@@ -1,16 +1,50 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class ElectricGun : MonoBehaviour {
+public class ElectricGun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public float impactForce = 10f;
+    public float fireReet = 15f;
 
-	// Use this for initialization
-	void Start () {
-		
+    public Camera fpsCam;
+    public ParticleSystem electricBall;
+    public GameObject impactEffect;
+
+    private float nextFire = 0f;
+
+	void Update ()
+    {
+		if (Input.GetButton("Fire1") && Time.time >= nextFire)
+        {
+            nextFire = Time.time + 1f / fireReet;
+            Shoot();
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void Shoot()
+    {
+        electricBall.Play();
+
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Enemy enemy = hit.transform.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
+        }
+
+    }
 }
